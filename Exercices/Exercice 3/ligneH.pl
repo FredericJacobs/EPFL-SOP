a90ae17e89b4ab5f5951efcc6491eb131aead131
Frederic Jacobs <github@fredericjacobs.com>
2014-05-24 01:15:30 +0200

    Parsing input files - WIP

--- Exercices/Exercice 3/ligneH.pl
+++ Exercices/Exercice 3/ligneH.pl
@@ -9,7 +9,7 @@ use strict;
 my $nArg = scalar @ARGV;
 
 # Define variables of files to be parsed and 'from' parameter
-my @files = [];
+my @files = ();
 my $from  = 0;
 
 if ($nArg == 0){
@@ -19,8 +19,12 @@ if ($nArg == 0){
 
 	foreach (@ARGV){
 		&parseArg($_);
-		#Each entry from the arguments is either a valid path or an option
 	}
+
+	foreach (@files){
+		&processFile($_)
+	}
+
 }
 
 
@@ -29,19 +33,17 @@ if ($nArg == 0){
 sub parseArg {
 
 	my $arg = $_[0];
-	
+
 	if($arg =~ m/^-from=/m){
 
 		if ($arg =~ /(?<=^-from=)([0-9]*)$/ ){
 			if (!$from){
 				$from = $1;
 			} else{
-				print "-from=X can only be defined once.";
-				exit 1;
+				die "-from=X can only be defined once.";
 			}
 		} else{
-			print "-from=x: x argument can't be parsed to a positive number";
-			exit 1;
+			die "-from=x: x argument can't be parsed to a positive number";
 		}
 
 	} else {
@@ -49,9 +51,39 @@ sub parseArg {
 		if (-e $arg) {
 			push(@files, $arg);
 		} else{
-			print "File $arg doesn't exist.";
-			exit 1;
+			die "File $arg doesn't exist.";
 		}
 
 	}
+}
+
+# The processFile subroutine parses a file
+
+sub processFile {
+
+	open(my $file, "<$_" ) or die "Can't open $_: $!\n";
+
+	print "Ligne d'horizon pour $_: \n";
+
+	my $FSM_state = 0;
+
+	while (<$file>) {
+		chomp;
+
+		my $number =()= $_ =~ /<immeuble /gi;
+
+		if($number){
+		#	print $_ =~ /<immeuble /."\n";
+		}
+
+		print $_."\n";
+		print $number."\n";
+
+	}
+
+	if ($FSM_state) {
+		die "File format of file $_ is not valid."
+	}
+
+	close $file;
 }
