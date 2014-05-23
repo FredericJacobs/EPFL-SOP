bbfcb9d05b7a75e33ef3e25637ce61fdfdeb3dd7
Frederic Jacobs <github@fredericjacobs.com>
2014-05-23 22:51:12 +0200

    Input parsing and sanitisation

--- Exercices/Exercice 3/ligneH.pl
+++ Exercices/Exercice 3/ligneH.pl
@@ -8,15 +8,15 @@ use strict;
 
 my $nArg = scalar @ARGV;
 
+# Define variables of files to be parsed and 'from' parameter
+my @files = [];
+my $from  = 0;
+
 if ($nArg == 0){
 	# read from Stdio
 	print "Reading from STDIO"
 } else{
-	
-	# Define variables of files to be parsed and 'from' parameter
-	my @files = [];
-	my @from  = 0;
-	
+
 	foreach (@ARGV){
 		&parseArg($_);
 		#Each entry from the arguments is either a valid path or an option
@@ -27,11 +27,31 @@ if ($nArg == 0){
 # The parseArg subroutine parses arguments from the command-line.
 
 sub parseArg {
-	# verify if matches from 
-	if ( ){
-		
-	}
+
+	my $arg = $_[0];
 	
-	# else it's file name, let's see if it exists
-	$_[0]
+	if($arg =~ m/^-from=/m){
+
+		if ($arg =~ /(?<=^-from=)([0-9]*)$/ ){
+			if (!$from){
+				$from = $1;
+			} else{
+				print "-from=X can only be defined once.";
+				exit 1;
+			}
+		} else{
+			print "-from=x: x argument can't be parsed to a positive number";
+			exit 1;
+		}
+
+	} else {
+
+		if (-e $arg) {
+			push(@files, $arg);
+		} else{
+			print "File $arg doesn't exist.";
+			exit 1;
+		}
+
+	}
 }
